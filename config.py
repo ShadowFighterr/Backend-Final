651d2b49b57bd7e58b9b9562ab8a05020854e0cf
Azamat <ubaidullauliazamat06@gmail.com>
2025-02-26 02:54:37 +0500

    set requirements.txt

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:@localhost/tasks'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:@localhost/tasks'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_static_secret_key')
+    SECRET_KEY = os.environ.get('SECRET_KEY', '265e9d1ef35635c99eed0ed3d95f7510')
     WTF_CSRF_ENABLED = True
